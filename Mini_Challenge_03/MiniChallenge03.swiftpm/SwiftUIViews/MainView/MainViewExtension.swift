ec3ed001b3dde551b65849e9f75f860b59f230fe
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-01-11 22:52:21 -0300

    arrumando as Stacks

--- Mini_Challenge_03/MiniChallenge03.swiftpm/SwiftUIViews/MainView/MainViewExtension.swift
+++ Mini_Challenge_03/MiniChallenge03.swiftpm/SwiftUIViews/MainView/MainViewExtension.swift
@@ -15,25 +15,18 @@ extension MainView{
     @ViewBuilder
     var shipView: some View {
         ZStack{
+            //backGround of the scene
             SpriteView(scene: mainView.backGroundScene)
                 .ignoresSafeArea()
                 .navigationBarBackButtonHidden()
             
             if mainView.shipAppear{
-                HStack{
-                    Spacer()
-                    VStack{
-                        Text("Hora atual: \(Date().formatted(date: .omitted, time: .complete))").colorInvert()
-                        Text("Hora da nave: ").colorInvert()
-                        Text("Diferenca: \(mainView.date.formatted(date: .omitted, time: .complete))").colorInvert()
-                        Spacer()
-                    }
-                    Spacer()
-                }
                 ObjectsSceneView(sceneSpeed: $mainView.shipSpeed, witchObject: $mainView.witchObject, canClearChat: $mainView.canClearChat)
                 
                 ShipSceneView(sceneSpeed: $mainView.shipSpeed, shipAppear: $mainView.shipAppear, witchObject: $mainView.witchObject, shipState: $mainView.shipState, canClearChat: $mainView.canClearChat, isLightSpeed: $mainView.isLightSpeed)
             }
+            
+            
             
             VStack{
                 if !mainView.shipAppear{
@@ -67,149 +60,161 @@ extension MainView{
                 }
             }
             
-            VStack{
-                Spacer()
-                HStack{
-                    Spacer()
-                    VStack{
-                        if mainView.shipAppear == true && mainView.shipState == "Middle"{
+            
+            
+            //mexendo
+            if mainView.shipAppear == true && mainView.shipState == "Middle"{
+                VStack{
+                    HStack{
+                        VStack{
+    //                                    Spacer()
+                            Text(Chats.upLeft.rawValue)
+                                .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
+                                .colorInvert()
+                            
+                            
+    //                                    Spacer()
+                            
+    //                                    Slider(value: $mainView.shipSpeed, in: 2.0...mainView.maxSpeed, step: 0.1)
+    //                                        .frame(width: 300)
+    //                                        .rotationEffect(.degrees(90))
+    ////                                        .padding()
+    //                                        .tint(.clear)
+                            
+                            Text("Normal")
+                                .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
+                                .foregroundStyle(.white)
+                            ZStack{
+                                let h: CGFloat = 200
+                                let w: CGFloat = 25
+                                
+                                let x = ((200 * mainView.shipSpeed) / 50) - 4
+                              
+                                
+                                Rec(y: 0)
+                                    .frame(width: w, height: h)
+                                    .foregroundColor(.clear)
+                                    .border(.white, width: 3)
+                                    
+                                Rec(y: x)
+                                    .frame(width: w * 0.87 , height: h * 0.98)
+                                    .foregroundColor(.red)
+                            }
+                            Text("∞")
+                                .font(.system(size: 30))
+                                .foregroundStyle(.white)
+                            
                             Spacer()
                             
-                            HStack{
+                        }
+                        
+                        Spacer()
+                       
+                        VStack{
+                            Text("Hora atual: \(Date().formatted(date: .omitted, time: .complete))").colorInvert()
+                            Text("Hora da nave: ").colorInvert()
+                            Text("Diferenca: \(mainView.date.formatted(date: .omitted, time: .complete))").colorInvert()
+                            Spacer()
+                        }
+                        
+                        Spacer()
+                        
+                        VStack{
+                            
+                            ZStack{
+                                SpeedCounter(startAngle: .degrees(0), endAngle: .degrees(300), clockWise: false)
+                                    .stroke(.gray, style: StrokeStyle(lineWidth: 10.5, lineCap: .round, lineJoin: .round))
+                                    .frame(width: 100, height: 100)
+                                
+                                SpeedCounter(startAngle: .degrees(0), endAngle: .degrees(Double(mainView.calculateShowSpeed(speed: mainView.shipSpeed))), clockWise: false)
+                                    .stroke(.linearGradient(colors: [/*Color(.color1), */Color(.color2), Color(.color3)], startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 10.5, lineCap: .round, lineJoin: .round))
+                                    .frame(width: 100, height: 100)
+                                
                                 VStack{
-//                                    Spacer()
-                                    Text(Chats.upLeft.rawValue)
+                                    Text("\(mainView.calculateRealSpeed(mainView.shipSpeed))")
                                         .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
                                         .colorInvert()
                                     
-                                    
-//                                    Spacer()
-                                    
-//                                    Slider(value: $mainView.shipSpeed, in: 2.0...mainView.maxSpeed, step: 0.1)
-//                                        .frame(width: 300)
-//                                        .rotationEffect(.degrees(90))
-////                                        .padding()
-//                                        .tint(.clear)
-                                    
-                                    Text("Normal")
+                                }
+                            }
+                            Text(Chats.upRight.rawValue)
+                            
+                            Spacer()
+                            
+                            //                                    Slider(value: $mainView.shipSpeed, in: 2.0...mainView.maxSpeed, step: 0.1)
+                            //                                        .frame(width: 300)
+                            //                                        .rotationEffect(.degrees(-90))
+                            //                                        .padding()
+                            
+                            
+                            if mainView.shipSpeed >= mainView.maxSpeed{
+                                Button {
+                                    mainView.isLightSpeed.toggle()
+                                } label:{
+                                    Text(Chats.lightSpeedButton.rawValue)
                                         .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
-                                        .foregroundStyle(.white)
-                                    ZStack{
-                                        let h: CGFloat = 200
-                                        let w: CGFloat = 25
-                                        
-                                        let x = ((200 * mainView.shipSpeed) / 50) - 4
-                                      
-                                        
-                                        Rec(y: 0)
-                                            .frame(width: w, height: h)
-                                            .foregroundColor(.clear)
-                                            .border(.white, width: 3)
-                                            
-                                        Rec(y: x)
-                                            .frame(width: w * 0.87 , height: h * 0.98)
-                                            .foregroundColor(.red)
-                                    }
-                                    Text("∞")
-                                        .font(.system(size: 30))
-                                        .foregroundStyle(.white)
-                                    
-                                    Spacer()
-                                    Button(action: {
-                                        
-                                    }, label: {
-                                        Text("Frear")
-                                            .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
-                                            .foregroundStyle(.white)
-                                    }).padding()
-                                    .onLongPressGesture(minimumDuration: 1) {
-                                        mainView.breakShip(canBreak: true)
-                                    } onPressingChanged: { inProgress in
-                                        
-                                        mainView.breakShip(canBreak: false)
-                                        
-                                        if inProgress {
-                                            mainView.breakShip(canBreak: true)
-                                        }else{
-                                            mainView.breakShip(canBreak: false)
-                                        }
-                                    }.padding()
-                                    
-                                    Spacer()
                                 }
+                            }else{
+                                Text(" ")
                                 
-                                Spacer()
                                 
-                                VStack{
-                                    
-                                    ZStack{
-                                        SpeedCounter(startAngle: .degrees(0), endAngle: .degrees(300), clockWise: false)
-                                            .stroke(.gray, style: StrokeStyle(lineWidth: 10.5, lineCap: .round, lineJoin: .round))
-                                            .frame(width: 100, height: 100)
-                                        
-                                        SpeedCounter(startAngle: .degrees(0), endAngle: .degrees(Double(mainView.calculateShowSpeed(speed: mainView.shipSpeed))), clockWise: false)
-                                            .stroke(.linearGradient(colors: [/*Color(.color1), */Color(.color2), Color(.color3)], startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 10.5, lineCap: .round, lineJoin: .round))
-                                            .frame(width: 100, height: 100)
-                                        
-                                        VStack{
-                                            Text("\(mainView.calculateRealSpeed(mainView.shipSpeed))")
-                                                .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
-                                                .colorInvert()
-                                            
-                                        }
-                                    }
-                                    Text(Chats.upRight.rawValue)
-                                    
-                                    Spacer()
-                                    
-//                                    Slider(value: $mainView.shipSpeed, in: 2.0...mainView.maxSpeed, step: 0.1)
-//                                        .frame(width: 300)
-//                                        .rotationEffect(.degrees(-90))
-//                                        .padding()
-                                    
-                                    
-                                    if mainView.shipSpeed >= mainView.maxSpeed{
-                                        Button {
-                                            mainView.isLightSpeed.toggle()
-                                        } label:{
-                                            Text(Chats.lightSpeedButton.rawValue)
-                                                .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
-                                        }
-                                    }else{
-                                        Text(" ")
-                                            
-                                       
-                                    }
-                                    
-                                    Button(action: {
-                                        
-                                    }, label: {
-                                        Text("Acelerar")
-                                            .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
-                                            .foregroundStyle(.white)
-                                    }).padding()
-                                    .onLongPressGesture(minimumDuration: 1) {
-                                        mainView.accelerateShip(canAccelerate: true)
-                                    } onPressingChanged: { inProgress in
-                                        
-                                        mainView.accelerateShip(canAccelerate: false)
-                                        
-                                        if inProgress {
-                                            mainView.accelerateShip(canAccelerate: true)
-                                        }else{
-                                            
-                                            mainView.accelerateShip(canAccelerate: false)
-                                        }
-                                    }.padding()
-                                    
-                                    Spacer()
-                                }
-                            }.padding()
+                            }
+                            
+                            
+                            Spacer()
                         }
+                    }
+                }.padding()
+                
+                VStack{
+                    HStack{
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Frear")
+                                .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
+                                .foregroundStyle(.white)
+                        }).padding()
+                        .onLongPressGesture(minimumDuration: 1) {
+                            mainView.breakShip(canBreak: true)
+                        } onPressingChanged: { inProgress in
+                            
+                            mainView.breakShip(canBreak: false)
+                            
+                            if inProgress {
+                                mainView.breakShip(canBreak: true)
+                            }else{
+                                mainView.breakShip(canBreak: false)
+                            }
+                        }.padding()
                         Spacer()
+                        
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            Text("Acelerar")
+                                .font(Font.custom(Chats.fontScene.rawValue, size: mainView.NormalFontSize))
+                                .foregroundStyle(.white)
+                        }).padding()
+                        .onLongPressGesture(minimumDuration: 1) {
+                            mainView.accelerateShip(canAccelerate: true)
+                        } onPressingChanged: { inProgress in
+                            
+                            mainView.accelerateShip(canAccelerate: false)
+                            
+                            if inProgress {
+                                mainView.accelerateShip(canAccelerate: true)
+                            }else{
+                                
+                                mainView.accelerateShip(canAccelerate: false)
+                            }
+                        }.padding()
+                        
                     }
                 }
             }
+            
         }.onAppear {
             Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
                 let timeDifference = mainView.calculateTimeDifference(speed: Double(mainView.calculateRealSpeed(mainView.shipSpeed)), currentTime: Date())
